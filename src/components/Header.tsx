293619c30065dc58a86721a2b23b5df3b98fd1ef
salba86-beep <salba_86@yahoo.it>
2026-01-29 17:04:57 +0000

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import albaLogo from "@/assets/alba-logo.png";
+import SalbaLogo from "@/assets/Salba-logo-5.png";
 
 const Header = () => {
   return (
@@ -12,7 +12,7 @@ const Header = () => {
         <div className="flex items-center justify-between h-16 md:h-20">
           <a href="#" className="flex items-center">
             <img 
-              src={albaLogo} 
+              src={SalbaLogo} 
               alt="SALBA Property Management" 
               className="h-14 md:h-16 w-auto"
             />
